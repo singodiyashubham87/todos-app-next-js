f821535031bd338de1ed1f910adae10fb5cec946
Shubham Singodiya <singodiyashubham87@gmail.com>
2025-02-16 22:57:07 +0530

    chore: add new todo creation logic

--- src/app/new/page.tsx
+++ src/app/new/page.tsx
@@ -1,7 +1,20 @@
+import { prisma } from "@/db";
 import Image from "next/image";
 import Link from "next/link";
+import { redirect } from "next/navigation";
 
-export default function NewTodo() {
+async function createTodo(data: FormData) {
+  "use server"; // server action
+  const todoTitle = data.get("title")?.valueOf();
+  if (typeof todoTitle !== "string" || todoTitle.length === 0) {
+    throw new Error("Invalid todo title");
+  }
+  await prisma.todo.create({
+    data: { title: todoTitle },
+  });
+  redirect('/')
+}
+export default async function NewTodo() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <header className="flex w-2/3 justify-between">
@@ -10,18 +23,21 @@ export default function NewTodo() {
         </Link>
       </header>
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <form className="flex flex-col gap-4 w-full">
+        <form className="flex flex-col gap-4 w-full" action={createTodo}>
           <label className="block font-medium text-gray-300">
             Todo Title
             <input
-              className="mt-1 text-gray-800 px-2 block w-full transition duration-200 ease-in-out outline-none rounded-md"
+              className="mt-1 text-gray-800 px-2 text-xl block w-full transition duration-200 ease-in-out outline-none rounded-md"
               type="text"
               name="title"
               placeholder="Enter title"
               required
             />
           </label>
-          <button className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44">
+          <button
+            type="submit"
+            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
+          >
             Add Todo
           </button>
         </form>
